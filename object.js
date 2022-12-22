c4408a1eb885bef42a048c03fffe1e5278dbc1fc
sabiya07 <mohammedsabiya2@gmail.com>
2022-12-23 01:49:49 +0530

    object.js

---
+++ object.js
@@ -0,0 +1,67 @@
+// let person = {
+//     age : '12',
+//     Name:"javascript",
+//     L_Name: "Language",
+
+//     phone_number:{
+//         mobile:"427863789",
+//         landline: "123434567"
+//     },
+
+//     callingMethod:function(){
+//         return (`the name of the person is ${person.Name}`)
+//     }
+// }
+// console.log(person.callingMethod());
+// console.log(person.phone_number.mobile);
+
+//------------------------------------------------------//
+// function Person(age,Name,L_Name){
+//     this.age=age;
+//     this.Name=Name;
+//     this.L_Name=L_Name;
+// }
+
+// function Person(L_Name){
+//     this.L_Name=L_Name;
+// }
+
+// let ob1=new person(21,"abdul")
+//--------------------------------------//
+// class vehicle{
+//     constructor(name,brand,color){
+//         this.name=name;
+//         this.brand=brand;
+//         this.color=color;
+
+//     }
+//     getname(){
+//         return (`the name of the vehicle is ${this.name}`)
+//     }
+
+//     getBrand(){
+//         rereturn (`the name of the vehicle is ${this.brand}`)
+//     }
+// }
+// let veh1=new vehicle("supra","toyota","black");
+//let veh2=new vehicle("swift","ms","blue")
+// console.log(veh1.name);
+//------------------------------
+// class bank {
+// constructor (employee_name,unique_id,branch_id){
+//     this.employee_name=employee_name;
+//     this.unique_id=unique_id;
+//     this.branch_id=branch_id;
+    
+//     }
+//     getDetails(){
+//         console.log(`Name:${this.employee_name}`);
+//         console.log(`Branch_id:${this.branch_id}`);
+//         console.log(`unique_id:${this.unique_id}`)
+//     }
+// }
+
+// let axis=new bank("gdfs",2345,1234);
+// console.log(axis.employee_name);     
+
+//----------------------------------------------------------------------//
