d678beb19a633c7665b9451b4adb5621b8892d88
sabiya07 <mohammedsabiya2@gmail.com>
2022-12-28 23:38:05 +0530

    array-2

---
+++ Arrays-2/sort0_1.js
@@ -0,0 +1,21 @@
+// let arr=[1,0,1,1,0];
+// sort(arr);
+// console.log(sort(arr))
+// function sort(arr){
+//       let count=0;
+//       for(let i=0;i<arr.length;i++){
+//         if(arr[i]==0)count++;
+
+//       }
+//       for(let i=0;i<count;i++){
+//         arr[i]=0;
+//       }
+//       for(let i=count;i<arr.length;i++){
+//         arr[i]=1;
+//       }
+//       return arr;
+// }
+
+
+
+
