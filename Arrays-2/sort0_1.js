f7eb036a65cf49735eb76e5978a6c31ad21a986e
sabiya07 <mohammedsabiya2@gmail.com>
2023-01-01 23:58:57 +0530

    arrays-2

--- Arrays-2/sort0_1.js
+++ Arrays-2/sort0_1.js
@@ -17,5 +17,41 @@
 // }
 
 
+ var n=13
+for(i=2;i<n;i++){
+if((n%i) ==0 ){
+    console.log("div by:",i);
+    console.log("then it is not  prime number")
+    break;
+}
+else {
+    console.log("div by:",i);
+    console.log("it is  a prime number")
+    break;
 
+}}
 
+//-----------------------------------
+function prime(n) {
+    if (n == 1) return false;
+    for (let i=2; i*i<=n; i++) {
+        if (n%i==0) 
+        return ( `${n} is Not a prime number`);
+        return (`${n} is a prime number`);
+    }
+}
+console.log(prime(7));
+
+//--------------------------------------------
+
+function isPrime(n){
+    if(n==1) return false;
+    if(n==2|| n==3)return true;
+    if(n%2==0 || n%3==0)return false;
+    for (let i=5; i+1<=n;i+=6){
+        if(n%i==0|| n%(i+2)==0) return false
+    }
+    return true;
+}
+
+//-------------------------------------------
